424eb4a7770f6eee94a3c836d35181fc05a1e6fb
nico-deve <114811339+nico-deve@users.noreply.github.com>
2024-04-21 23:52:11 +0200

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-// für Navigation 
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const listContainer = document.getElementsByClassName('button-container')[0]
-
-toggleButton.addEventListener('click', () => {
-    listContainer.classList.toggle('active')
-});
-
-
